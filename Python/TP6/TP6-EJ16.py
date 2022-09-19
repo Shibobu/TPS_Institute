4d116f3cf718807974c161fe5314c64975167c99
Shibobu <agawelove@gmail.com>
2022-09-19 20:37:27 -0300

    Ejercicio 16 finalizado

--- Python/TP6/TP6-EJ16.py
+++ Python/TP6/TP6-EJ16.py
@@ -13,10 +13,54 @@ indicar cuál es el importe a pagar por el consumo eléctrico en dicha residenci
 def ing_float(arg):
     return float(input(arg))
 
+def ing_int(arg):
+    return int(input(arg))
+
 def ingreso(arg):
     cons = ing_float(arg) 
     return cons if(cons > 0) else ingreso("Datos erroneos, ingrese nuevamente: ")
 
+def ingreso_int(arg):
+    num = ing_int(arg)
+    return num if(num>0 & num <= 3) else ingreso_int("Datos erroneos, Ingrese nuevamente: ")
+
+def prod(num1,num2):
+    return num1*num2
+
+def comp_cons(cons):
+    match cons:
+                case cons if(cons <= 130): return 0
+                case cons if(130 < cons < 500): return 1
+                case cons if(500 < cons < 700): return 2
+                case default: return 3
+
+def match_estacion(cons):
+    op = ingreso_int("---Elija la estacion del año actual---"
+                    +"\n1----------Invierno"
+                    +"\n2----------Verano"
+                    +"\n3----------Primavera u Otoño"
+                    +"\nOpcion: ")
+                    
+    invierno = [0.04,0.08,0.11,0.16]
+    verano = [0.04,0.11,0.13,0.26]
+    privoto= [0,0.03,0.02,0.10]
+
+    match op:
+        case 1:
+            cons = prod(cons,invierno[comp_cons(cons)])
+        case 2:
+            cons = prod(cons,verano[comp_cons(cons)])
+        case default:
+            cons = prod(cons,privoto[comp_cons(cons)])
+            
+        
+    salida(cons)
+
+def salida(cons):
+    print("El consumo de la casa es:",cons)
+
 def p_consumoHogar():
     cons = ingreso("Ingrese el consumo de una casa: ")
-    
+    match_estacion(cons)
+
+p_consumoHogar()
