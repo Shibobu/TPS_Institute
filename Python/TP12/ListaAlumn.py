9d6fb87aa45241cfc8682e6d86adaae2156bb040
Shibobu <agawelove@gmail.com>
2022-10-28 01:36:51 -0300

    TP12

--- Python/TP12/ListaAlumn.py
+++ Python/TP12/ListaAlumn.py
@@ -5,11 +5,16 @@ Manejo de la lista de alumnos
 from Mylibrary import *
 from Alumnos import *
 
-alumnos = []
+alumnos = {}
 
-def askWTC():
-    x = ing_str("Desea seguir ingresando?\n Y/y para si\n N/n para no\n Ingrese Y/y o N/n: ").upper
-    return True if x == 'Y' else False
+def askWTC(arg):
+    x = ing_str(arg).upper()
+    if x == 'Y':
+        return True
+    elif x == 'N':
+        return False
+    else:
+        askWTC("Error, ingrese correctamente \n Ingrese Y/y o N/n: ")
 
 def ing_alum():
     while True:
@@ -19,12 +24,29 @@ def ing_alum():
         telefono = ing_str("Ingrese el Telefono del alumno: ")
         correo = ing_str("Ingrese el Correo del alumno: ")
 
-        alumnos.append(ing_alum(dni,nomb,apellido,telefono,correo))
-
-        if not askWTC():
+        alumnos[dni] = ini_alum(nomb,apellido,telefono,correo)
+        if not askWTC("Desea seguir ingresando?\n Y/y para si\n N/n para no\n Ingrese Y/y o N/n: "):
             break
 
-def elim_alumn():
-    for e in alumnos: 
-        if e.get('DNI')
-        
+def elim_alumn(bus : int):
+    try:
+        del alumnos[bus]
+    except:
+        print("O nyo")
+
+def show_alum(dnib : int):
+    try:
+        if alumnos[dnib]!= 'None':
+            print("Dni: {dni}".format(dni = dnib))
+            show(alumnos.get(dnib))
+    except:
+        print("No se encontro el alumno")
+
+def show_all():
+    for key, value in alumnos.items():
+        print("Dni: {d}".format(d=key))
+        show(value)
+
+if __name__ == "__main__":
+    ing_alum()
+    show_all()
