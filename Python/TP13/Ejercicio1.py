e2d857796162d310e0b93d50f6e66d340e3335ee
Shibobu <agawelove@gmail.com>
2022-11-03 20:33:26 -0300

    update

---
+++ Python/TP13/Ejercicio1.py
@@ -0,0 +1,11 @@
+"""
+Ejercicio 1:
+Crear un script que lea los datos de un archivo de texto del tipo .csv con al menos 5 filas 
+con la misma estructura del Trabajo Práctico Nro.12 (DNI, nombre, dirección, teléfono, 
+correo electrónico); cabe destacar que al mostrar los datos deberán estar con su 
+correspondiente nombre de columna
+Ejemplo:
+DNI:26452458, Nombre: Jesús González, Dirección: Alvarado 850, Teléfono:
+387585698, Correo Electrónico: ingjesusgonzalez@yahoo.com.ar
+DNI:3659874, Nombre:………….., Dirección:…………..
+"""
